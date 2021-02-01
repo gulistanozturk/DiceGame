4511d4b4c9e084d094518a06254fb71d2e13b453
Gülistan Öztürk <gulistanozturk137@gmail.com>
2021-02-01 20:03:01 +0300

    Update Crag.java

--- ZarOyunu/src/Crag.java
+++ ZarOyunu/src/Crag.java
@@ -5,7 +5,7 @@
             Die die1 = new Die();
             Die die2 = new Die();
             Die die3 = new Die();
-           
+
             die1.roll();
             die2.roll();
             die3.roll();
